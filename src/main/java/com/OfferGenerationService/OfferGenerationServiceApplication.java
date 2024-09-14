17a17a15e9b5c9cbb4fe5521e923fff35ae4bc85
Shabbir khan <33508921+shabbirkhan0015@users.noreply.github.com>
2024-09-14 15:19:29 +0530

    firstcommit

---
+++ src/main/java/com/OfferGenerationService/OfferGenerationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.OfferGenerationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfferGenerationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfferGenerationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/OfferGenerationService/OfferGenerationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OfferGenerationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfferGenerationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
